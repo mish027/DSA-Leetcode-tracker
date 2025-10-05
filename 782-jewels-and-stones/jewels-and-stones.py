3993a2a452d1aaa7e8c96ccaaa5964e4cb1f770c
mish027 <124340098+mish027@users.noreply.github.com>
2025-10-05 19:11:21 +0530

    Time: 0 ms (100.00%) | Memory: 17.6 MB (82.72%) - LeetSync

--- 782-jewels-and-stones/jewels-and-stones.py
+++ 782-jewels-and-stones/jewels-and-stones.py
@@ -8,4 +8,14 @@ class Solution:
                 count+=1
         return count
 
-    
+    '''Time Complexity:
+    - Creating the set jewelsSet from the list jewels takes O(J) time, where J is the number of jewels.
+    - Iterating through the list stones takes O(S) time, where S is the number of stones.
+    - Checking if a stone is in jewelsSet is an O(1) operation due to the hash set.
+    - Overall, the total time complexity is O(J + S).
+
+    Space Complexity:
+    - The set jewelsSet requires O(J) space to store the jewels.
+    - The other variables use constant space.
+    - Overall, the space complexity is O(J).
+    '''
