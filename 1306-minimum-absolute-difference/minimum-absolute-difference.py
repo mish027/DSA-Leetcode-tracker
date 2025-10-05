db21a96850f6b6ca4363224de6fd1bd94dcba8ad
mish027 <124340098+mish027@users.noreply.github.com>
2025-10-05 19:30:24 +0530

    Time: 43 ms (93.72%) | Memory: 29.8 MB (99.20%) - LeetSync

--- 1306-minimum-absolute-difference/minimum-absolute-difference.py
+++ 1306-minimum-absolute-difference/minimum-absolute-difference.py
@@ -7,15 +7,12 @@ class Solution:
 
         for i in range(len(arr)-1):
             diff=abs(arr[i+1]-arr[i])
-            if diff<=minDiff:
+            if diff<minDiff:
                 minDiff=diff
-        for i in range(len(arr)-1):
-            diff=abs(arr[i+1]-arr[i])
-            if diff==minDiff:
+                result=[[arr[i],arr[i+1]]]
+       
+            elif diff==minDiff:
                 result.append([arr[i],arr[i+1]])
 
         return result
-
-
-        return arr
         
