9ad29754bbe74c79b256b649f791d85043a039e3
mish027 <124340098+mish027@users.noreply.github.com>
2025-10-05 19:51:29 +0530

    Time: 0 ms (100.00%) | Memory: 18.4 MB (69.88%) - LeetSync

--- 898-transpose-matrix/transpose-matrix.py
+++ 898-transpose-matrix/transpose-matrix.py
@@ -1,10 +1,13 @@
 class Solution:
     def transpose(self, matrix: List[List[int]]) -> List[List[int]]:
 
-        result=[[0]*len(matrix) for i in  range(len(matrix[0]))]
+        result=[[0]*len(matrix) for i in  range(len(matrix[0]))] #gives len(matrix) columns and 1st element length rows
         for i in range(len(matrix)):
             for j in range(len(matrix[0])):
                 result[j][i]=matrix[i][j]
 
         return result
-        
+    
+    '''The time complexity of this transpose function is O(m * n), where m is the number of rows in the input matrix and n is the number of columns. This is because the nested loops iterate over each element of the matrix exactly once, performing a constant-time assignment operation for each element.
+
+The space complexity is also O(m * n). The function creates a new result matrix with dimensions n (rows of the original matrix) by m (columns of the original matrix). This additional space is proportional to the size of the input matrix, as it stores the transposed data.'''
