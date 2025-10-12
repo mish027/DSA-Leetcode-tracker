5731bffedae86dfc78139a889af059b64cfc5fad
mish027 <124340098+mish027@users.noreply.github.com>
2025-10-12 19:13:46 +0530

    Time: 19 ms (96.76%) | Memory: 26.9 MB (77.92%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -1,11 +1,17 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         
+        #bruteforce approach TC:O(n^sq) and SC:O(1)
+        '''for i in range(len(arr)):
+            for j in range(i+1,len(arr)):
+                diff=prices[i]-prices[j]
+                if diff>0 and diff>maxProt'''
+        
+
+        #optimised approach TC:O(n) and SC:O(1)
         low=float('inf')
         maxProfit=0
-
-        for price in prices:
-
+        for price in prices:                        
             if price<low:
                 low=price
             elif price-low>maxProfit:
