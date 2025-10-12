a927c09476fd6cfdd282fc6a18ff310abc509804
mish027 <124340098+mish027@users.noreply.github.com>
2025-10-12 18:08:09 +0530

    Time: 8 ms (37.27%) | Memory: 19.4 MB (18.94%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,19 +1,18 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
 
-        hashMap={}
         #better bruteforce approach:
+        hashMap={}
         for num in nums:
             if num in hashMap:
                 hashMap[num]+=1
             else:
                 hashMap[num]=1
+                
         for num,count in hashMap.items():
             if count>len(nums)//2:
                 return num
             
-
-
         #optimised approach: 
         '''res=0
         majority=0
