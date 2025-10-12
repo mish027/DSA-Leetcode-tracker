ac97b9b2cf4d75217c8dc3c9368501386693a688
mish027 <124340098+mish027@users.noreply.github.com>
2025-10-12 20:37:57 +0530

    Time: 4 ms (3.82%) | Memory: 17.9 MB (16.27%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.py
+++ 118-pascals-triangle/pascals-triangle.py
@@ -2,9 +2,6 @@ class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
 
         mat=[[0]*(i+1) for i in range(numRows)]
-        '''for i in range(numRows):
-            for j in range(i+1):
-                mat[i][j]=0'''
 
         for i in range(numRows):
             for j in range(i+1):
