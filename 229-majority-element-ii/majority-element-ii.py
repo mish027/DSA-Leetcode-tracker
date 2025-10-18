48538d0beb9cf25247dbdd3681eb780c07c20688
mish027 <124340098+mish027@users.noreply.github.com>
2025-10-18 19:02:45 +0530

    Time: 11 ms (30.06%) | Memory: 22.3 MB (51.43%) - LeetSync

---
+++ 229-majority-element-ii/majority-element-ii.py
@@ -0,0 +1,23 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> List[int]:
+
+        #bruteforce approach:
+
+        res=[]
+        hashMap={}
+        for num in nums:
+            if num not in hashMap:
+                hashMap[num]=0
+            hashMap[num]+=1
+        
+        for item,count in hashMap.items():
+            if count>(len(nums)//3):
+                res.append(item)
+        return res
+
+
+
+
+
+        
+        
