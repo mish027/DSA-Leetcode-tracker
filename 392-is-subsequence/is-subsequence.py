9d11df8799f23691c168929554d71b23e73ef5d7
mish027 <124340098+mish027@users.noreply.github.com>
2025-10-22 23:18:17 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (24.55%) - LeetSync

--- 392-is-subsequence/is-subsequence.py
+++ 392-is-subsequence/is-subsequence.py
@@ -3,20 +3,13 @@ class Solution:
         
         i=0
         j=0
-        flag=0
-
-        if not s:
-            return True
 
         while i<len(s) and j<len(t):
             if s[i]==t[j]:
                 i+=1
-                j+=1
-                flag=1
-            elif s[i]!=t[j]:
-                j+=1
+            j+=1
         
-        if i==len(s) and flag==1:
+        if i==len(s):
             return True
         return False
         
