a46336518bcbeae56cccee42bce352afa67def59
mish027 <124340098+mish027@users.noreply.github.com>
2026-03-16 08:34:52 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (10.84%) - LeetSync

--- 231-power-of-two/power-of-two.c
+++ 231-power-of-two/power-of-two.c
@@ -1,6 +1,11 @@
 bool isPowerOfTwo(int n) {
 
-    for (int i=0; i<=(log(n)/log(2));i++){
+    if (n<=0){
+        return false;
+    }
+    int limit = log(n)/log(2); //value never changes, so recalculation is not needed
+
+    for (int i=0; i<=limit;i++){
 
         if (n==pow(2,i)){
             return true;
