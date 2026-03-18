c74b8e0ee26161f05a53d8816f6fba5dc9826f2e
mish027 <124340098+mish027@users.noreply.github.com>
2026-03-18 19:10:27 +0530

    Time: 1 ms (96.22%) | Memory: 42.7 MB (51.41%) - LeetSync

---
+++ 231-power-of-two/power-of-two.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+
+        //using recursion:
+
+            if (n==0){
+                return false;
+            }
+            
+            if (n%2==0){
+                return isPowerOfTwo(n/2);
+            }
+            if (n==1){
+                return true;
+            }
+            return false;
+        
+        }
+        
+    }
