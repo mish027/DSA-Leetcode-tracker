ae77d48861a387e3c4e378e2f717aeaed1bca5d0
mish027 <124340098+mish027@users.noreply.github.com>
2026-03-28 22:35:40 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (46.98%) - LeetSync

--- 504-base-7/base-7.py
+++ 504-base-7/base-7.py
@@ -1,24 +1,28 @@
 class Solution:
     def convertToBase7(self, num: int) -> str:
 
+        if num==0:
+            return "0"
         s = ""
 
-        if num<0:
-            nu=-num
-        else:
-            nu = num
+        nu = abs(num)
 
         while nu>0:
             remain=nu%7
             s = str(remain)+s
             nu=nu//7
 
-        if num==0:
-            return "0"
+        
         if num<0:
             return "-" + s
         return s
 
         #mistake:
         # didnt consider -ve numbers
-        # return '-'+ s 
+        # return '-'+ s  only when num<0
+        # cleaner version to use abs() instead of 
+        '''if num<0:
+            nu=-num
+        else:
+            nu = num'''
+        # add 0 case in the first
