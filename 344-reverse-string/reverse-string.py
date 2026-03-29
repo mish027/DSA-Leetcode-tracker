a9ae6a83dfdd9c729de6f6213b76ca31dcd8d314
mish027 <124340098+mish027@users.noreply.github.com>
2026-03-29 13:21:41 +0530

    Time: 7 ms (10.85%) | Memory: 23.4 MB (62.76%) - LeetSync

---
+++ 344-reverse-string/reverse-string.py
@@ -0,0 +1,26 @@
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        #using stack
+        myStack=[]
+        for ch in s:
+            myStack.append(ch)
+
+        k=len(myStack)
+        for i in range(k):
+
+            if myStack:
+                s[i]=myStack.pop()
+        return s
+
+
+        # using two pointer swap
+        '''l=0
+        h=len(s)-1
+        while l<h:
+            s[l],s[h]=s[h],s[l]
+            l+=1
+            h-=1
+        return "".join(s)'''
